0821f47d833e54ce64eedcd1ee685d7d5228bd3d
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2024-03-29 23:29:54 +0900

    [Silver V] Title: 집합, Time: 1192 ms, Memory: 313040 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,41 +1,54 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
         int M = Integer.parseInt(br.readLine());
-        int s = 0;
+        int S = 0;  // 집합
 
-        for (int i = 0; i < M; i++) {
-            String[] input = br.readLine().split(" ");
-            switch (input[0]) {
+        // M회 반복
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            int num = 0;
+
+            switch (str) {
+                case "add":
+                    // 0~19로 표현 해야 하기 때문에 -1
+                    num = Integer.parseInt(st.nextToken()) - 1;
+                    // or 연산 -> num번째 비트만 1로
+                    S = S | (1 << num);
+                    break;
+                case "remove":
+                    num = Integer.parseInt(st.nextToken()) - 1;
+                    // and 연산 -> num번째 비트만 0으로
+                    S = S & ~(1 << num);
+                    break;
+                case "check":
+                    num = Integer.parseInt(st.nextToken()) - 1;
+                    // num 번째 비트만 1로 -> temp 사용
+                    int temp = S & (1 << num);
+                    // 만약 0이면 없음
+                    sb.append(((temp == 0) ? 0 : 1) + "\n");
+                    break;
+                case "toggle":
+                    num = Integer.parseInt(st.nextToken()) - 1;
+                    // xor 연산 진행 -> 0이면 1로, 1이면 0으로
+                    S = S ^ (1 << num);
+                    break;
                 case "all":
-                    s = (1 << 21) - 1;
+                    S = (1 << 21) - 1;
                     break;
                 case "empty":
-                    s = 0;
+                    S = 0;
                     break;
-                default:
-                    int x = Integer.parseInt(input[1]);
-                    switch (input[0]) {
-                        case "add":
-                            s |= (1 << x);
-                            break;
-                        case "remove":
-                            s &= ~(1 << x);
-                            break;
-                        case "check":
-                            sb.append((s & (1 << x)) != 0 ? 1 : 0).append('\n');
-                            break;
-                        case "toggle":
-                            s ^= (1 << x);
-                            break;
-                    }
             }
         }
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
