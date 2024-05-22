1af8d32e2b9e04bc287bb7a0dc243edf100ab787
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2024-05-23 03:33:17 +0900

    [Gold V] Title: 괄호의 값, Time: 124 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+        String input = bfr.readLine();
+
+        Stack<Character> stk = new Stack<>();
+        int result = 0;
+        int tmp = 1;
+
+        L1: for(int i=0; i<input.length(); i++){
+            char c = input.charAt(i);
+
+            switch(c){
+                case '(':
+                    stk.add('(');
+                    tmp *= 2;
+                    break;
+
+                case '[':
+                    stk.add('[');
+                    tmp *= 3;
+                    break;
+
+                case ')':
+                    if(stk.isEmpty() || stk.peek()!='('){
+                        result = 0;
+                        break L1;
+                    }
+                    else{
+                        if(input.charAt(i-1)=='(')  result += tmp;
+                        stk.pop();
+                        tmp /= 2;
+                    }
+                    break;
+
+                case ']':
+                    if(stk.isEmpty() || stk.peek()!='['){
+                        result = 0;
+                        break L1;
+                    }
+                    else{
+                        if(input.charAt(i-1)=='[')  result += tmp;
+                        stk.pop();
+                        tmp /= 3;
+                    }
+                    break;
+            }
+        }
+
+        if(!stk.isEmpty()) System.out.println(0);
+        else System.out.println(result);
+    }
+}
