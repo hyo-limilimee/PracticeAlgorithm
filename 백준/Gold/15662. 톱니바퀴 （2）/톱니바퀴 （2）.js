bb23979d33090ed13350801168a231a5b8edbcba
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2024-07-05 23:51:10 +0900

    [Gold V] Title: 톱니바퀴 (2), Time: 196 ms, Memory: 15108 KB -BaekjoonHub

--- 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.js
+++ 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.js
@@ -2,12 +2,57 @@ const fs = require("fs");
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const T = Number(input[0]);
-let gears = input.slice(1, T + 1).map(line => line.split('').map(Number));
-const K = Number(input[T + 1]);
-let rotations = input.slice(T + 2, T + 2 + K).map(line => line.split(' ').map(Number));
+const N = Number(input[0]);
+let gears = input.slice(1, N + 1).map(line => line.split('').map(Number));
+const K = Number(input[N + 1]);
+let rotations = input.slice(N + 2, N + 2 + K).map(line => line.split(' ').map(Number));
 
-function rotateGear(gear, direction) {
+function solution() {
+    for (let i = 0; i < K; i++) {
+        let gearIndex = rotations[i][0] - 1;
+        let direction = rotations[i][1];
+
+        let rotateDirection = Array(N).fill(0);
+        rotateDirection[gearIndex] = direction;
+
+        // 왼쪽
+        for (let j = gearIndex - 1; j >= 0; j--) {
+            if (gears[j][2] !== gears[j + 1][6]) {
+                rotateDirection[j] = -rotateDirection[j + 1];
+            } else {
+                break;
+            }
+        }
+
+        // 오른쪽
+        for (let j = gearIndex + 1; j < N; j++) {
+            if (gears[j][6] !== gears[j - 1][2]) {
+                rotateDirection[j] = -rotateDirection[j - 1];
+            } else {
+                break;
+            }
+        }
+
+        // 회전
+        for (let k = 0; k < N; k++) {
+            if (rotateDirection[k] !== 0) {
+                gears[k] = rotate(gears[k], rotateDirection[k]);
+            }
+        }
+
+    }
+
+    let cnt = 0;
+    for (let j = 0; j < gears.length; j++) {
+        if (gears[j][0] === 1) {
+            cnt++;
+        }
+    }
+
+    return cnt;
+}
+
+function rotate(gear, direction) {
     if (direction === 1) {
         return [gear[7], ...gear.slice(0, 7)];
     } else {
@@ -15,48 +60,4 @@ function rotateGear(gear, direction) {
     }
 }
 
-function solution() {
-    for (let i = 0; i < K; i++) {
-        let gearIndex = rotations[i][0] - 1;
-        let direction = rotations[i][1];
-        
-        let rotateDirs = Array(T).fill(0);
-        rotateDirs[gearIndex] = direction;
-        
-        // Check left side
-        for (let j = gearIndex - 1; j >= 0; j--) {
-            if (gears[j][2] !== gears[j + 1][6]) {
-                rotateDirs[j] = -rotateDirs[j + 1];
-            } else {
-                break;
-            }
-        }
-        
-        // Check right side
-        for (let j = gearIndex + 1; j < T; j++) {
-            if (gears[j][6] !== gears[j - 1][2]) {
-                rotateDirs[j] = -rotateDirs[j - 1];
-            } else {
-                break;
-            }
-        }
-        
-        // Apply rotations
-        for (let j = 0; j < T; j++) {
-            if (rotateDirs[j] !== 0) {
-                gears[j] = rotateGear(gears[j], rotateDirs[j]);
-            }
-        }
-    }
-    
-    let count = 0;
-    for (let j = 0; j < T; j++) {
-        if (gears[j][0] === 1) {
-            count++;
-        }
-    }
-    
-    console.log(count);
-}
-
-solution();
+console.log(solution());
