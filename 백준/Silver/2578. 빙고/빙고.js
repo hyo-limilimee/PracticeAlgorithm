586cc2a59a55ce90f8005cdd716552a3bb60660e
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2024-07-05 21:46:05 +0900

    [Silver IV] Title: 빙고, Time: 96 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.js
+++ 백준/Silver/2578. 빙고/빙고.js
@@ -2,25 +2,22 @@ const fs = require("fs");
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().split('\n');
 
-const inputs = input.map((nums) => nums.split(' ').map((num) => Number(num)));
-const bingo = inputs.slice(0, 5); // 빙고판
-const target = inputs.slice(5, 10); // target 요소
+const bingo = input.slice(0, 5).map(line => line.split(' ').map(Number));
+const called = input.slice(5, 10).map(line => line.split(' ').map(Number));
 
 let x = [0, 0, 0, 0, 0];
 let y = [0, 0, 0, 0, 0];
 let xy = [0, 0];
 
-console.log(solution());
-
 function solution() {
     let cnt = 0;
 
-    // target 순회
     for (let i = 0; i < 5; i++) {
         for (let j = 0; j < 5; j++) {
-            // target 순회하며 bingo판 숫자 지우기
-            del(target[i][j]);
-            cnt++;  // 숫자 카운트 증가
+            // bingo의 숫자 지우기
+            delBingo(called[i][j]);
+            cnt++;
+
             if (checkBingo()) {
                 return cnt;
             }
@@ -28,48 +25,17 @@ function solution() {
     }
 }
 
-function checkBingo() {
-    let bingoCnt = 0;
-    for (let i = 0; i < 5; i++) {
-        if (x[i] === -5) {
-            bingoCnt++;
-
-            if (bingoCnt >= 3) {
-                return true;
-            }
-        }
-
-        if (y[i] === -5) {
-            bingoCnt++;
-
-            if (bingoCnt >= 3) {
-                return true;
-            }
-        }
-    }
-
-    for (let i = 0; i < 2; i++) {
-        if (xy[i] <= -5) {
-            bingoCnt++;
-            if (bingoCnt >= 3) {
-                return true;
-            }
-        }
-    }
-}
-
-// 빙고판 숫자 지내기
-function del(delNum) {
+function delBingo(targetNum) {
     for (let i = 0; i < 5; i++) {
         for (let j = 0; j < 5; j++) {
-            if (bingo[i][j] == delNum) {
-                bingo[i][j] = -1;   // 빙고판 지우기
-                // 해당 줄에 -1씩 
-                x[i] += -1;
-                y[j] += -1;
+            if (targetNum === bingo[i][j]) {
+                bingo[i][j] = -1;
+                x[i]++;
+                y[j]++;
                 checkXY(i, j);
                 return;
             }
+
         }
     }
 }
@@ -77,12 +43,43 @@ function del(delNum) {
 function checkXY(i, j) {
     for (let a = 0; a < 5; a++) {
         if (i === a && j === a) {
-            xy[0] += -1;
+            xy[0]++;
         }
     }
+
     for (let b = 0; b < 5; b++) {
         if (i === b && j === 4 - b) {
-            xy[1] += -1;
+            xy[1]++;
         }
     }
 }
+
+function checkBingo() {
+    let bingoCnt = 0;
+    for (let i = 0; i < 5; i++) {
+        if (x[i] >= 5) {
+            bingoCnt++;
+
+        }
+
+        if (y[i] === 5) {
+            bingoCnt++;
+
+        }
+
+        if (bingoCnt >= 3) {
+            return true;
+        }
+    }
+
+    for (let j = 0; j < 2; j++) {
+        if (xy[j] === 5) {
+            bingoCnt++;
+            if (bingoCnt >= 3) {
+                return true;
+            }
+        }
+    }
+}
+
+console.log(solution());
