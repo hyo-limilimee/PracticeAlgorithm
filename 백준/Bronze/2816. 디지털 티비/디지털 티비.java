15b2b91df551b441ad059fcd70d4d877b63748be
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2024-12-19 22:54:18 +0900

    [Bronze I] Title: 디지털 티비, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/2816. 디지털 티비/디지털 티비.java
+++ 백준/Bronze/2816. 디지털 티비/디지털 티비.java
@@ -1,45 +1,53 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String[] arr = new String[n];
-        for(int i = 0 ; i<n; i++) {
-        	arr[i] = sc.next();
+    static String[] channels;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        channels = new String[N];
+
+        // 채널 목록 입력
+        for (int i = 0; i < N; i++) {
+            channels[i] = br.readLine();
         }
-        
-       	for(int i = 0 ;i<n; i++) {
-       		if(arr[i].equals("KBS1")) {//KBS1부터 첫번째로
-       			String tmp = "";
-       			for(int j = i; j>0; j--) {
-        			System.out.print(4);
-        			tmp = arr[j-1];
-        			arr[j-1] = arr[j];
-        			arr[j] = tmp;
-       			}
-       			break;
-        	}
-        	else 
-        		System.out.print(1);
-       	}
-       	
-       	if(arr[1].equals("KBS2"))//KBS1을 첫번째로 옮기고 만약 KBS2가 두번째에 있다면 종료
-        	System.exit(0);
-        	
-       	for(int i = 0 ;i<n; i++) {
-       		if(arr[i].equals("KBS2")) {//KBS2를 두번쨰로
-       			String tmp = "";
-       			for(int j = i; j>1; j--) {
-       				System.out.print(4);
-       				tmp = arr[j-1];
-       				arr[j-1] = arr[j];
-        			arr[j] = tmp;
-        		}
-        		break;
-       		}
-       		else
-       			System.out.print(1);
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            if (channels[i].equals("KBS1")) { // KBS1부터 첫번째로
+                for (int j = i; j > 0; j--) {
+                    result.append(4);
+                    swap(j, j - 1);
+                }
+                break;
+            } else {
+                result.append(1);
+            }
         }
+
+        if (channels[1].equals("KBS2")) // KBS1을 첫번째로 옮기고 만약 KBS2가 두번째에 있다면 종료
+            System.out.print(result);
+        else {
+            for (int i = 0; i < N; i++) {
+                if (channels[i].equals("KBS2")) { // KBS2를 두번째로
+                    for (int j = i; j > 1; j--) {
+                        result.append(4);
+                        swap(j, j - 1);
+                    }
+                    break;
+                } else {
+                    result.append(1);
+                }
+            }
+            System.out.print(result);
+        }
+    }
+
+    private static void swap(int a, int b) {
+        String tmp = channels[a];
+        channels[a] = channels[b];
+        channels[b] = tmp;
     }
 }
