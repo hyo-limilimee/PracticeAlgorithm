a95cf5ddf8bcfecc193437b641d4772b03b70914
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-02-28 18:58:51 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 14.32 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,19 +1,17 @@
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        
         int remainDelivery = 0;
         int remainPickup = 0;
-        
-        // 뒤에서부터 탐색
-        for(int i = n - 1; i >= 0; i--){
+      
+        for(int i = n- 1; i >= 0; i--){
             remainDelivery += deliveries[i];
             remainPickup += pickups[i];
             
             while(remainDelivery > 0 || remainPickup > 0){
                 remainDelivery -= cap;
                 remainPickup -= cap;
-
+                
                 answer += (i + 1) * 2;
             }
         }
