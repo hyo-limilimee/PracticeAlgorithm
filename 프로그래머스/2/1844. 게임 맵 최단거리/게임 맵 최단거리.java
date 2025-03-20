a48c06cd4620b94354e2ef5c08fdb6f111fbc184
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-03-21 03:12:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.02 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,74 +1,40 @@
 import java.util.*;
 
-class Node {
-    int x;
-    int y;
-    int count;
-
-    public Node(int x, int y, int count) {
-        this.x = x;
-        this.y = y;
-        this.count = count;
-    }
-}
-
 class Solution {
-    static int[][] maps;
-    static boolean[][] visited;
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
+    int[] dx = {-1, 0, 1, 0};  
+    int[] dy = {0, 1, 0, -1};
     
     public int solution(int[][] maps) {
-        this.maps = maps;
-        int answer = 0;
-        visited = new boolean[maps.length][maps[0].length];
+        int row = maps.length;  // 행 (세로)
+        int col = maps[0].length;  // 열 (가로)
         
-        answer = bfs(0,0);
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {0, 0});  
         
-        if(answer == 0){
-            answer = -1;
-        }
-        
-        return answer;
-    }
-    
-    public int bfs(int x, int y){
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(x,y,1));
+        boolean[][] visited = new boolean[row][col];
         visited[0][0] = true;
+        maps[0][0] = 1;  
         
         while(!queue.isEmpty()){
-            Node node = queue.poll();
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
             
-            if(node.x == maps.length - 1 && node.y == maps[0].length - 1){
-                return node.count;
+            if(x == row - 1 && y == col - 1){
+                return maps[x][y];  
             }
             
             for(int i = 0; i < 4; i++){
-                int nx = node.x + dx[i];
-                int ny = node.y + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                if(nx >=0 && nx < maps.length && ny >= 0 && ny < maps[0].length){
-                    if(!visited[nx][ny] && maps[nx][ny]!=0){
-                        queue.add(new Node(nx, ny, node.count+1));
-                        visited[nx][ny] = true;
-                    }
+                if(nx >= 0 && ny >= 0 && nx < row && ny < col && !visited[nx][ny] && maps[nx][ny] == 1){
+                    visited[nx][ny] = true;
+                    maps[nx][ny] = maps[x][y] + 1; 
+                    queue.offer(new int[] {nx, ny});
                 }
             }
         }
-        return 0;
+        return -1;  
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
