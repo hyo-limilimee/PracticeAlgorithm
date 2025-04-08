5c49b00599dc6dc72fbfe2d456ff0369bb926a23
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-04-08 16:53:00 +0900

    [Silver II] Title: 부분수열의 합, Time: 116 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,46 +1,40 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static int cnt;
-    public static int N;
+    static int N;
+    static int S;
+    static int[] arr;
+    static int count = 0;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        arr = new int[N];
 
-        int[] numbers = new int[21];
-        cnt = 0;
         st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(numbers, 0, S, 0);
-
+        makeSum(0, 0);
         if (S == 0) {
-            System.out.println(cnt - 1);
-            return;
+            count--;
         }
-        System.out.println(cnt);
+        System.out.println(count);
     }
 
-    public static void dfs(int[] numbers, int depth, int target, int sum) {
-        // 마지막 노드까지 탐색한 경우
-        if (depth == N) {
-            if (target == sum) {
-                cnt++;
+    private static void makeSum(int index, int sum) {
+        if (index == N) {
+            if (sum == S) {
+                count++;
             }
             return;
-        } else {
-            dfs(numbers, depth + 1, target, sum + numbers[depth]);
-            dfs(numbers, depth + 1, target, sum);
         }
 
+        makeSum(index + 1, sum + arr[index]); // 현재 수 포함
+        makeSum(index + 1, sum);            // 현재 수 미포함
     }
-
 }
