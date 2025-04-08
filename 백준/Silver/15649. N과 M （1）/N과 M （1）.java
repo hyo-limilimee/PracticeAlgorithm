0ff84b758cbfe6b0a49d5f172db329588d113781
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-04-08 16:21:18 +0900

    [Silver III] Title: N과 M (1), Time: 236 ms, Memory: 21952 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,45 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    static int N;
+    static int M;
+    static int[] picked;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
 
-    public static int[] arr;
-    public static boolean[] visited;
-    public static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        arr = new int[M];
-        visited = new boolean[N];
-        dfs(N, M, 0);
+        picked = new int[M];
+        visited = new boolean[N + 1];
+        pick(0);
         System.out.println(sb);
-
     }
 
-    public static void dfs(int N, int M, int depth) {
-
+    private static void pick(int depth) {
         if (depth == M) {
-            for (int val : arr) {
-                sb.append(val).append(' ');
+            for (int i = 0; i < M; i++) {
+                sb.append(picked[i]).append(" ");
             }
-            sb.append('\n');
+            sb.append("\n");
             return;
         }
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                arr[depth] = i + 1;
-                dfs(N, M, depth + 1);
+                picked[depth] = i;
+                pick(depth + 1);
                 visited[i] = false;
             }
         }
