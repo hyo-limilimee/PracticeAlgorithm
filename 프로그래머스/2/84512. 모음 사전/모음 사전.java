c46debd0329f0fd6eaeb8373bd6aa0d658206d38
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-05-17 17:08:28 +0900

    [level 2] Title: 모음 사전, Time: 0.04 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -5,13 +5,8 @@ class Solution {
         int[] count = {781, 156, 31, 6, 1};
         
         for(int i = 0; i < word.length(); i++){
-            System.out.println(word.charAt(i));
-            
             int vowelIdx = vowel.indexOf(word.charAt(i));
-            System.out.println(vowelIdx);
-            
             answer += vowelIdx * count[i];
-            System.out.println(answer);
         }
        
         return answer + word.length();
