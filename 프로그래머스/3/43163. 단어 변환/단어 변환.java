57f6d5d58122a2b73b0c5899aea08072254638f7
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-05-17 18:49:15 +0900

    [level 3] Title: 단어 변환, Time: 0.55 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    private class Word{
+        String word;
+        int depth;
+        
+        Word(String word, int depth){
+            this.word = word;
+            this.depth = depth;
+        }
+    }
+    
+    private boolean isAdjacent(String a, String b){
+        int diff = 0;
+        
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i) != b.charAt(i)){
+                diff++;
+                if(diff > 1) return false;
+            }
+        }
+        return diff == 1;
+    }
+    
+    public int solution(String begin, String target, String[] words) {
+        boolean[] visited = new boolean[words.length];
+        Queue<Word> queue = new LinkedList<>();
+        queue.offer(new Word(begin, 0));
+        
+        while(!queue.isEmpty()){
+            Word current = queue.poll();
+            
+            if(current.word.equals(target)){
+                return current.depth;
+            }
+            
+            for(int i = 0; i < words.length; i++){
+                if(!visited[i] && isAdjacent(current.word, words[i])){
+                    visited[i] = true;
+                    queue.offer(new Word(words[i], current.depth + 1));
+                }
+            }
+        }
+        return 0;
+    }
+}
