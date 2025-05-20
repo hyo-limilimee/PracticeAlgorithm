1ee0f700675c20a12a5d7926a00e517cc877364d
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-05-20 22:59:42 +0900

    [level 3] Title: 여행경로, Time: 0.70 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,28 +1,30 @@
 import java.util.*;
 
 class Solution {
-    private List<String> path = new ArrayList<>();
-    private boolean[] visited;
-    private String[][] tickets;
+    String[][] tickets;
+    List<String> path = new ArrayList<>();
+    boolean[] visited;
     private List<String> answer = null;
+    
     
     public String[] solution(String[][] tickets) {
         this.tickets = tickets;
         visited = new boolean[tickets.length];
         
         Arrays.sort(tickets, (a, b) -> {
-           if(a[0].equals(b[0])){
-               return a[1].compareTo(b[1]);
-           }else{
-               return a[0].compareTo(b[0]);
-           }
+            if(a[0].equals(b[0])){
+                return a[1].compareTo(b[1]);
+            }else{
+                return a[0].compareTo(b[0]);
+            }
         });
-        
+            
         path.add("ICN");
         dfs("ICN", 0);
         
         return answer.toArray(new String[0]);
     }
+    
     
     private void dfs(String current, int depth){
         if(depth == tickets.length){
@@ -30,17 +32,17 @@ class Solution {
             return;
         }
         
-        for(int i = 0; i < tickets.length; i++){
-            if(!visited[i] && tickets[i][0].equals(current)){
-                visited[i] = true;
-                path.add(tickets[i][1]);
-                
-                dfs(tickets[i][1], depth + 1);
-                if(answer != null) return;
-                
-                visited[i] = false;
-                path.remove(path.size() - 1);
-            }
-        }
+       for(int i = 0; i < tickets.length; i++){
+           if (!visited[i] && tickets[i][0].equals(current)){
+               visited[i] = true;
+               path.add(tickets[i][1]);
+               
+               dfs(tickets[i][1], depth + 1);
+               if(answer != null) return;
+               
+               visited[i] = false;
+               path.remove(path.size() - 1);
+           }
+       }
     }
 }
