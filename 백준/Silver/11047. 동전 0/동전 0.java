cc58870104cf5747f07b0b786f23ecab0869084e
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-05-22 18:10:59 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -4,26 +4,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-
-        Integer[] coins = new Integer[N];
+        int[] A = new int[N];
 
         for (int i = 0; i < N; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
+            A[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(coins, Collections.reverseOrder());
-
         int cnt = 0;
-
-        for (int i = 0; i < coins.length; i++) {
-            if (K / coins[i] > 0) {
-                cnt += K / coins[i];
-                K = K % coins[i];
-            }
+        for (int i = N - 1; i >= 0; i--) {
+            cnt += K / A[i];
+            K = K % A[i];
         }
         System.out.println(cnt);
     }
