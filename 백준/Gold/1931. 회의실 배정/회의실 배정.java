ef796cc9053bf6ccb40e9f1e79a5a71cc10834ba
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-05-22 17:46:33 +0900

    [Gold V] Title: 회의실 배정, Time: 536 ms, Memory: 43596 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][] time;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        time = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, (a, b) -> {
+            if (a[1] == b[1]) {
+                return a[0] - b[0];
+            }
+            return a[1] - b[1];
+        });
+
+        int count = 0;
+        int prev = 0;
+
+        for (int i = 0; i < N; i++) {
+            int start = time[i][0];
+            int end = time[i][1];
+
+            if (start >= prev) {
+                count++;
+                prev = end;
+            }
+        }
+        System.out.println(count);
+    }
+}
