75c583950f229e173ce6996e3a5679dc420eaacf
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-08-01 14:17:53 +0900

    [Bronze II] Title: 16진수, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        String numStr = br.readLine();
+        int num = Integer.parseInt(numStr, 16);
+        System.out.println(num);
+    }
+}
