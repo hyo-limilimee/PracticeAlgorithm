b5f62812ac532b19705338cc781067cd717958d9
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-11-12 20:47:14 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 168 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibo(n));
+    }
+
+    private static int fibo(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        return fibo(n-1) + fibo(n-2);
+    }
+}
