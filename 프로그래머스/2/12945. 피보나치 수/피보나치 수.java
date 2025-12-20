1f443081e5500725ee6bcc05a3c048a2a14033bf
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-12-20 15:07:14 +0900

    [level 2] Title: 피보나치 수, Time: 3.71 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int MOD = 1234567;
+        int[] fibo = new int[n + 1];
+
+        fibo[0] = 0;
+        fibo[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibo[i] = (fibo[i - 1] + fibo[i - 2]) % MOD;
+        }
+
+        return fibo[n];
+    }
+}
