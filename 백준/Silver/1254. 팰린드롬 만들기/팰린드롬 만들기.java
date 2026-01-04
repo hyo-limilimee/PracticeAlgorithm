7d9a626e0f8690dacc72163e836d054c3c939966
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2026-01-04 21:31:53 +0900

    [Silver II] Title: 팰린드롬 만들기, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int n = str.length();
+
+        for (int i = 0; i < n; i++) {
+            int left = i;
+            int right = n - 1;
+            boolean ok = true;
+
+            while (left < right) {
+                if (str.charAt(left) != str.charAt(right)) {
+                    ok = false;
+                    break;
+                }
+
+                left++;
+                right--;
+            }
+
+            if (ok) {
+                System.out.println(n + i);
+                return;
+            }
+
+        }
+    }
+}
