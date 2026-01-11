1b6d1aeb500700279fe52039fda3117e4061060c
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2026-01-11 21:21:38 +0900

    [level 2] Title: 숫자의 표현, Time: 0.61 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n) {
+        int count = 0;
+        int left = 1, right = 1;
+        int sum = 1;
+
+        while (left <= n) {
+            if (sum == n) {
+                count++;
+                sum -= left;
+                left++;
+            } else if (sum < n) {
+                right++;
+                sum += right;
+            } else {
+                sum -= left;
+                left++;
+            }
+        }
+
+        return count;
+    }
+}
